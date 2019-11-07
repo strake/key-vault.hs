06d86521c10c6ae36c3773819573f7b777429ef0
M Farkas-Dyck <strake888@gmail.com>
2019-11-07 15:04:09 -0800

    Start

---
+++ src/Data/Vault.hs
@@ -0,0 +1,14 @@
+module Data.Vault (Vault, alterF) where
+
+import Control.Monad.Trans.Key
+import Data.Map (Map)
+import qualified Data.Map as Map
+import GHC.Exts (Any)
+import Numeric.Natural
+import Unsafe.Coerce
+
+newtype Vault s = Vault { unVault :: Map Natural Any }
+
+alterF :: (Functor f) => (Maybe a -> KeyringT s f (Maybe a)) -> Key s a -> Vault s -> KeyringT s f (Vault s)
+alterF f k = fmap Vault . Map.alterF f' (unsafeCoerce k) . unVault
+  where f' = unsafeCoerce f :: Maybe Any -> KeyringT s f (Maybe Any)
