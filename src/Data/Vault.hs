ae9648b0e86d77733b5116d2c64ef4b4453e6ddf
M Farkas-Dyck <strake888@gmail.com>
2019-11-08 14:13:18 -0800

    empty, adjustA

--- src/Data/Vault.hs
+++ src/Data/Vault.hs
@@ -1,14 +1,19 @@
-module Data.Vault (Vault, alterF) where
+module Data.Vault (Vault, adjustA, empty, alterF) where
 
 import Control.Monad.Trans.Key
 import Data.Map (Map)
-import qualified Data.Map as Map
+import qualified Data.Map.Class as Map
 import GHC.Exts (Any)
 import Numeric.Natural
 import Unsafe.Coerce
 
-newtype Vault s φ = Vault { unVault :: Map Natural Any }
+newtype Vault s a = Vault { unVault :: Map Natural Any }
 
-alterF :: (Functor f) => (Maybe (φ a) -> f (Maybe (φ a))) -> Key s a -> Vault s φ -> f (Vault s φ)
-alterF f k = fmap Vault . Map.alterF f' (unsafeCoerce k) . unVault
-  where f' = unsafeCoerce f :: Maybe Any -> f (Maybe Any)
+adjustA :: Applicative p => (a k -> p (a k)) -> Key s k -> Vault s a -> p (Vault s a)
+adjustA f k = fmap Vault . Map.adjustA (unsafeCoerce f) (unsafeCoerce k) . unVault
+
+empty :: Vault s a
+empty = Vault Map.empty
+
+alterF :: (Functor f) => (Maybe (a k) -> f (Maybe (a k))) -> Key s k -> Vault s a -> f (Vault s a)
+alterF f k = fmap Vault . Map.alterF (unsafeCoerce f) (unsafeCoerce k) . unVault
--- src/Data/Vault/Identity.hs
+++ src/Data/Vault/Identity.hs
@@ -8,5 +8,8 @@ import qualified Data.Vault as V
 
 type Vault s = V.Vault s Identity
 
+adjustA :: Applicative p => (a -> p a) -> Key s a -> Vault s -> p (Vault s)
+adjustA f = V.adjustA (fmap coerce . f . coerce)
+
 alterF :: (Functor f) => (Maybe a -> f (Maybe a)) -> Key s a -> Vault s -> f (Vault s)
 alterF f = V.alterF (fmap coerce . f . coerce)
